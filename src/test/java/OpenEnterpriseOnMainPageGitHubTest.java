de9b5470eec2563fe4d39cbee1c930d6e53e5233
vskoshkin <Jumpykoshkin@gmail.com>
2024-01-19 10:45:04 +0300

    HomeWork done Selenide 2 v 2

--- src/test/java/OpenEnterpriseOnMainPageGitHubTest.java
+++ src/test/java/OpenEnterpriseOnMainPageGitHubTest.java
@@ -6,10 +6,10 @@ import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class OpenEnterpriseOnMainPageGitHub {
+public class OpenEnterpriseOnMainPageGitHubTest {
 
     @Test
-    void openEnterprise() {
+    void openEnterpriseTest() {
         open("https://github.com/");
         $("header.Header-old.header-logged-out").$(byText("Solutions")).hover();
         $(byTagAndText("a", "Enterprise")).click();
